acb095200b23ddd787cdb64a5a084581425caac7
Magali MAI <maimagali26.pro@gmail.com>
2025-08-22 11:44:50 +0200

    Remove the store temporary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,15 @@
 import { Stack } from "expo-router";
-import { Provider } from "react-redux";
-import { store } from "@stores/index";
+// import { Provider } from "react-redux";
+// import { store } from "@stores/index";
 
 export default function RootLayout() {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
+    // </Provider>
       <Stack>
         <Stack.Screen name="splash/index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </Provider>
   );
 }
