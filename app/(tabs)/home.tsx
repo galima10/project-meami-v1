f0690324f1d75416197e489c4ce4092f07bc4c53
Magali MAI <maimagali26.pro@gmail.com>
2025-08-22 22:22:13 +0200

    Prepare the debug apk to test the feature and build apk for the merge into main

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>HOME</Text>
+      <Text style={styles.text}>HOME</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "SN",
+    fontWeight: "700",
+  },
+});
