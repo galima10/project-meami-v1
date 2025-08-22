d8a83d23d7d61925ff9a92e6ca8f0913e6228f31
Magali MAI <maimagali26.pro@gmail.com>
2025-08-22 11:42:15 +0200

    Add a wave text animation into the splash screen

---
+++ hooks/animations/useWaveAnimation.ts
@@ -0,0 +1,31 @@
+// useWaveAnimation.js
+import { useSharedValue, useAnimatedStyle, withRepeat, withTiming, Easing } from "react-native-reanimated";
+import { useEffect } from "react";
+
+export const useWaveAnimation = (index:number, { amplitude = 10, speed = 3000 } = {}) => {
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withRepeat(
+      withTiming(2 * Math.PI, {
+        duration: speed,
+        easing: Easing.linear,
+      }),
+      -1,
+      false
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const offset = index * 0.18;
+    return {
+      transform: [
+        {
+          translateY: Math.sin(progress.value + offset) * amplitude,
+        },
+      ],
+    };
+  });
+
+  return animatedStyle;
+};
