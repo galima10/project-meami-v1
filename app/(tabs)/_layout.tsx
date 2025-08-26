58539793b4ea06d23d47ae693fe19c3dc54e2b6c
Magali MAI <maimagali26.pro@gmail.com>
2025-08-26 12:49:27 +0200

    Create a TabBar component with functionnal links

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,40 @@
 import { Tabs } from "expo-router";
+import TabBar from "@components/molecules/tabBar/TabBar";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="menu"
-        options={{
-          headerShown: false,
-          tabBarStyle: { display: "none" },
-        }}
-      />
-      <Tabs.Screen
-        name="cart"
-        options={{
-          headerShown: false,
-          tabBarStyle: { display: "none" },
-        }}
-      />
-      <Tabs.Screen
-        name="stock"
-        options={{
-          headerShown: false,
-          tabBarStyle: { display: "none" },
-        }}
-      />
-      <Tabs.Screen
-        name="infos"
-        options={{
-          headerShown: false,
-          tabBarStyle: { display: "none" },
-        }}
-      />
-    </Tabs>
+    <>
+      <Tabs>
+        <Tabs.Screen
+          name="menu"
+          options={{
+            headerShown: false,
+            tabBarStyle: { display: "none" },
+          }}
+        />
+        <Tabs.Screen
+          name="cart"
+          options={{
+            headerShown: false,
+            tabBarStyle: { display: "none" },
+          }}
+        />
+        <Tabs.Screen
+          name="stock"
+          options={{
+            headerShown: false,
+            tabBarStyle: { display: "none" },
+          }}
+        />
+        <Tabs.Screen
+          name="infos"
+          options={{
+            headerShown: false,
+            tabBarStyle: { display: "none" },
+          }}
+        />
+      </Tabs>
+      <TabBar />
+    </>
   );
 }
---
+++ components/atoms/tabBar/TabButton.tsx
@@ -0,0 +1,37 @@
+import { Pressable, Text, StyleSheet } from "react-native";
+import theme from "@themes/index";
+import { Link } from "expo-router";
+import { usePathname } from "expo-router";
+
+interface TabButtonProps {
+  routeName: "menu" | "cart" | "stock" | "infos";
+  label: string;
+}
+
+const TabButton = ({ routeName, label }: TabButtonProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === `/${routeName}`;
+  return (
+    <Link href={`/${routeName}`} asChild>
+      <Pressable>
+        <Text style={{ color: isActive ? "red" : "black" }}>{label}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    // padding: 12,
+    backgroundColor: "#6200ee",
+    borderRadius: 8,
+    alignItems: "center",
+    flex: 1,
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+});
+
+export default TabButton;
