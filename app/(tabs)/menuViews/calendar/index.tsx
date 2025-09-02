0e5420d4b91880eff82980702ae7cfa14b55acee
Magali MAI <maimagali26.pro@gmail.com>
2025-09-02 13:23:50 +0200

    Reupdate momentselected when goBack on the screen

--- app/(tabs)/menuViews/calendar/index.tsx
+++ app/(tabs)/menuViews/calendar/index.tsx
@@ -4,22 +4,28 @@ import { getDateInfo } from "@utils/getDate";
 import DayContainer from "@components/organisms/calendar/DayContainer";
 import MomentModule from "@components/organisms/calendar/MomentModule";
 import { useDayMoment } from "@hooks/dayMoment/useDayMoment";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useInteractionCooldown } from "@hooks/calendar/useInteractionCooldown";
 import DayNavigation from "@components/organisms/calendar/DayNavigation";
+import { useDate } from "@hooks/dayMoment/useDate";
+import { AppText } from "@components/atoms/global/Texts";
+import { useFocusEffect } from "@react-navigation/native";
 
 export default function CalendarView() {
   const { dayOfWeek, dayAndMonth } = getDateInfo();
-  const { actualDayMoment } = useDayMoment();
+  const { hour } = useDate();
+  const { actualDayMoment, displayMoment } = useDayMoment();
   const [momentSelected, setMomentSelected] = useState(actualDayMoment);
 
   const { hasInteracted, handleInteraction } = useInteractionCooldown();
 
-  useEffect(() => {
+  useFocusEffect(
+  useCallback(() => {
     if (!hasInteracted) {
       setMomentSelected(actualDayMoment);
     }
-  }, [actualDayMoment, hasInteracted]);
+  }, [actualDayMoment, hasInteracted])
+);
 
   return (
     <View
@@ -34,6 +40,13 @@ export default function CalendarView() {
           momentSelected={momentSelected}
           setMomentSelected={setMomentSelected}
         />
+        <AppText style={{ marginTop: 20, textTransform: "capitalize", color: "white" }}>
+          {dayOfWeek}
+        </AppText>
+        <AppText style={{ color: "white" }}>{dayAndMonth}</AppText>
+        <AppText style={{ color: "white" }}>
+          {hour}h : {displayMoment}
+        </AppText>
       </View>
     </View>
   );
