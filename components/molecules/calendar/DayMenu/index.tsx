3d95383f1e2b9ce5af41af4e9618368d98da0083
Magali MAI <maimagali26.pro@gmail.com>
2025-09-04 13:03:46 +0200

    Add a LinearGradient background

--- components/molecules/calendar/DayMenu/index.tsx
+++ components/molecules/calendar/DayMenu/index.tsx
@@ -1,18 +1,27 @@
 import { View, StyleSheet } from "react-native";
 import { AppText } from "@components/atoms/global/Texts";
-
+import { LinearGradient } from "expo-linear-gradient";
+import theme from "@themes/index";
 export default function DayMenu() {
   return (
     <View style={styles.container}>
-      <AppText>Texte</AppText>
+      {/* <AppText>Texte</AppText> */}
+      <LinearGradient
+        colors={["transparent", "#fc4545ca"]}
+        locations={[0.5, 1]}
+        style={{ flex: 1, width: "100%", justifyContent: "center", alignItems: "center" }}
+      >
+        <AppText style={{fontSize: 20 }}>
+          Hello gradient!
+        </AppText>
+      </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 16,
-    backgroundColor: "white",
+    backgroundColor: theme.properties.beige,
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
