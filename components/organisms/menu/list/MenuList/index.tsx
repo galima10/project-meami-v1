1a9e8aa3ab92776579f1b66194e0e0eedfdc6e3d
Magali MAI <maimagali26.pro@gmail.com>
2025-09-05 16:55:52 +0200

    Add a empty menu for test

--- components/organisms/menu/list/MenuList/index.tsx
+++ components/organisms/menu/list/MenuList/index.tsx
@@ -4,6 +4,46 @@ import { days } from "@utils/getDate";
 import { mockedMenu } from "@constants/mockedMenu";
 
 export default function MenuList() {
+  const mockedMenuEmpty: Record<
+    string,
+    { matin: string[]; midi: string[]; soir: string[] }
+  > = {
+    lundi: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+    mardi: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+    mercredi: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+    jeudi: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+    vendredi: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+    samedi: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+    dimanche: {
+      matin: [],
+      midi: [],
+      soir: [],
+    },
+  };
   return (
     <ScrollView
       style={styles.menuList}
@@ -11,7 +51,7 @@ export default function MenuList() {
       contentContainerStyle={{ paddingBottom: 16 }}
     >
       {days.map((day) => (
-        <MenuElementList key={day} day={day} menu={mockedMenu[day]} />
+        <MenuElementList key={day} day={day} menu={mockedMenuEmpty[day]} />
       ))}
     </ScrollView>
   );
