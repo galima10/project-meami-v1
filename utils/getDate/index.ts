2d4be6bf39aa6a0d52deaf0d12d411cc2738b9d2
Magali MAI <maimagali26.pro@gmail.com>
2025-09-10 10:21:50 +0200

    Change the moment condition

--- utils/getDate/index.ts
+++ utils/getDate/index.ts
@@ -50,7 +50,7 @@ export function getDateInfo() {
 }
 
 export function getDayMoment(hour: number): "morning" | "noon" | "evening" {
-  if (hour >= 4 && hour < 12) return "morning";
+  if (hour >= 0 && hour < 12) return "morning";
   if (hour >= 12 && hour < 18) return "noon";
   return "evening";
 }
