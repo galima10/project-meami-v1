98c6240acbf28ab4d6877cef57c563516d78135f
Magali MAI <maimagali26.pro@gmail.com>
2025-09-10 16:13:33 +0200

    Add the day slider for the modify view

--- app/(tabs)/menuViews/modify/index.tsx
+++ app/(tabs)/menuViews/modify/index.tsx
@@ -1,14 +1,37 @@
-import { View, StyleSheet, ImageBackground } from "react-native";
+import { View, StyleSheet, ImageBackground, ScrollView } from "react-native";
+import { days } from "@utils/getDate";
 import DaySlide from "@components/organisms/menu/modify/DaySlide";
+import { useRef, useState } from "react";
+import { getScreenWidth } from "@utils/getScreenDimensions";
 
 export default function ModifyView() {
+  const scrollRef = useRef<ScrollView>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
     <ImageBackground
       source={require("@assets/images/precharged/background/menu_3x.jpg")}
       style={styles.screen}
       resizeMode="cover"
     >
-      <DaySlide />
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ref={scrollRef}
+        scrollEnabled={true}
+        pagingEnabled
+        onMomentumScrollEnd={(e) => {
+          const offsetX = e.nativeEvent.contentOffset.x;
+          const newIndex = Math.round(offsetX / getScreenWidth());
+          setCurrentIndex(newIndex);
+        }}
+      >
+        {days.map((day, index) => (
+          <View key={index} style={{ width: getScreenWidth(), flex: 1 }}>
+            <DaySlide key={day} day={day} />
+          </View>
+        ))}
+      </ScrollView>
     </ImageBackground>
   );
 }
--- components/organisms/menu/modify/DaySlide/index.tsx
+++ components/organisms/menu/modify/DaySlide/index.tsx
@@ -1,19 +1,44 @@
 import { AppText } from "@components/atoms/global/Texts";
 import theme from "@themes/index";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import AppButton from "@components/molecules/global/AppButton";
 import MomentElement from "./MomentElement";
 
-export default function DaySlide() {
+interface DaySlideProps {
+  day?: string;
+}
+
+export default function DaySlide({ day }: DaySlideProps) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <AppButton icon="trash" type="secondary" iconSize={40} style={styles.trashButton} />
-        <AppText style={styles.dayTitle}>Dimanche</AppText>
+        <AppButton
+          icon="trash"
+          type="secondary"
+          iconSize={40}
+          style={styles.trashButton}
+        />
+        <AppText style={styles.dayTitle}>{day}</AppText>
       </View>
-      <MomentElement moment="Matin" />
-      <MomentElement moment="Midi" />
-      <MomentElement moment="Soir" />
+      <ScrollView
+        style={styles.momentContainer}
+        contentContainerStyle={{ paddingBottom: 16 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <MomentElement moment="Matin" />
+        <MomentElement moment="Midi" />
+        <MomentElement moment="Soir" />
+
+        {/* <MomentElement moment="Matin" />
+        <MomentElement moment="Midi" />
+        <MomentElement moment="Soir" />
+        <MomentElement moment="Matin" />
+        <MomentElement moment="Midi" />
+        <MomentElement moment="Soir" />
+        <MomentElement moment="Matin" />
+        <MomentElement moment="Midi" />
+        <MomentElement moment="Soir" /> */}
+      </ScrollView>
     </View>
   );
 }
@@ -22,7 +47,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    paddingHorizontal: 16,
     paddingVertical: 8,
   },
   header: {
@@ -31,7 +55,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     position: "relative",
     width: "100%",
-    marginBottom: 8,
   },
   dayTitle: {
     fontSize: 36,
@@ -42,7 +65,12 @@ const styles = StyleSheet.create({
   },
   trashButton: {
     position: "absolute",
-    left: 8,
+    left: 24,
     padding: 4,
+  },
+  momentContainer: {
+    width: "100%",
+    paddingHorizontal: 16,
+    paddingTop: 8,
   },
 });
